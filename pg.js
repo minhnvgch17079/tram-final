6bc2bead1450e1cbdab83a831293c375912eeca6
minhnvgch17079 <minhnvgch17079@fpt.edu.vn>
2019-08-29 12:17:09 +0700

    fix ssql

--- pg.js
+++ pg.js
@@ -1,10 +1,11 @@
 const {Client, Pool} = require('pg')
 const client = new Client({
-    database: 'tram',
-    host: 'localhost',
-    user: 'postgres',
-    password: '2108',
-    port: 5433
+    database: 'df38uhr2n5kgfo',
+    host: 'ec2-50-16-197-244.compute-1.amazonaws.com',
+    user: 'uikznrlazcqvje',
+    password: '6e9cd7ed1c27294d105d007ca1020bd0490a23282322a387590b99da5e57450b',
+    port: 5432,
+    ssl: true
 })
 
 client.connect()
